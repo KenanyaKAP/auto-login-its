0b9097e72139b27154687599af00928b69d79944
KenanyaKAP <69131654+KenanyaKAP@users.noreply.github.com>
2024-08-28 23:10:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def startBot(username, password, url):
     op.add_argument('headless')
     driver = webdriver.Chrome(options=op)
 
+    print("Try to login")
     driver.get(url)
     driver.find_element(
         value="username").send_keys(username)
@@ -17,22 +18,28 @@ def startBot(username, password, url):
         value="password").send_keys(password)
     driver.find_element(
         value="login").click()
+    time.sleep(3)
 
     if "incorrect" in driver.page_source:
         print("Incorrect credentials!")
+        return
+
+    driver.get(url)
+    time.sleep(3)
+
+    if "Anda telah terhubung" in driver.page_source:
+        print("Done login")
     else:
-        driver.get(url)
-        if "Anda telah terhubung" in driver.page_source:
-            print("Done login")
-        else:
-            print("Login failed, please try again")
+        print("Last loaded page")
+        print("===================================================")
+        print(driver.page_source)
+        print("===================================================")
+        print("Login failed, please try again!")
 
 # Enter below your login credentials
-username = "50xxxxxxxx"
+username = "5024xxxxxx"
 password = "password"
 
 url = "https://myits-app.its.ac.id/internet/auth.php"
 
 startBot(username, password, url)
-
-print("Auto login ends")
